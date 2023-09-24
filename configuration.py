4d65379ecdd2babd9555550c298bd0714e1f46b6
Anastasiia <gultceva-ai@yandex.ru>
2023-09-24 21:00:16 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://502fe4c4-a2f6-449a-9fc2-a2ed87690773.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
